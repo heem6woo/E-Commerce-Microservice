de6f003fd4822d91d32c4a40578dd5a514ea30c6
ooANAoo <rkawkaos77@naver.com>
2024-04-17 16:24:20 +0900

    redis import

--- SellerLogin/src/main/java/com/example/login/security/RedisConfig.java
+++ SellerLogin/src/main/java/com/example/login/security/RedisConfig.java
@@ -3,6 +3,9 @@ package com.example.login.security;
 import lombok.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
 public class RedisConfig {

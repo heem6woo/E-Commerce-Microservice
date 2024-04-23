0e41994ad39951f69406463bf4d76db171eb3d83
ooANAoo <rkawkaos77@naver.com>
2024-04-23 14:32:50 +0900

    feat itemRepository 최적화

--- itemservice/src/main/java/com/ecommerce/itemservice/repository/ItemRepository.java
+++ itemservice/src/main/java/com/ecommerce/itemservice/repository/ItemRepository.java
@@ -11,77 +11,49 @@ import java.util.List;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<Item, Integer> {
-
-    @Query("SELECT new com.ecommerce.itemservice.dto.ItemDTO(" +
+    String joinQuery = "SELECT new com.ecommerce.itemservice.dto.ItemDTO(" +
             "new com.ecommerce.itemservice.dto.ItemInfo(i.itemId, c.categoryId, i.itemName, i.regDt), " +
             "new com.ecommerce.itemservice.dto.SalesInfoDTO(s.salesInfoId, s.sellerId, s.itemCount, s.itemPrice, s.itemStatus)) " +
             "FROM Item i " +
             "JOIN i.category c " +
-            "JOIN i.salesInfo s " +
-            "WHERE i.itemName LIKE concat('%', :name, '%')")
+            "JOIN i.salesInfo s ";
+    String whereName = "WHERE i.itemName LIKE concat('%', :name, '%')";
+    
+    @Query( joinQuery+
+            whereName)
     List<ItemDTO> findItemsByNameLike(@Param("name") String name);
-    @Query("SELECT new com.ecommerce.itemservice.dto.ItemDTO(" +
-            "new com.ecommerce.itemservice.dto.ItemInfo(i.itemId, c.categoryId, i.itemName, i.regDt), " +
-            "new com.ecommerce.itemservice.dto.SalesInfoDTO(s.salesInfoId, s.sellerId, s.itemCount, s.itemPrice, s.itemStatus)) " +
-            "FROM Item i " +
-            "JOIN i.category c " +
-            "JOIN i.salesInfo s " +
-            "WHERE i.itemName LIKE concat('%', :name, '%') " +
+    @Query(joinQuery +
+            whereName +
             "AND c.categoryId = :catId "
     )
     List<ItemDTO> findItemsByNameLikeAndSameCat(@Param("name") String name,@Param("catId") short catId );
 
-    @Query("SELECT new com.ecommerce.itemservice.dto.ItemDTO(" +
-            "new com.ecommerce.itemservice.dto.ItemInfo(i.itemId, c.categoryId, i.itemName, i.regDt), " +
-            "new com.ecommerce.itemservice.dto.SalesInfoDTO(s.salesInfoId, s.sellerId, s.itemCount, s.itemPrice, s.itemStatus)) " +
-            "FROM Item i " +
-            "JOIN i.category c " +
-            "JOIN i.salesInfo s " +
-            "WHERE i.itemName LIKE concat('%', :name, '%') " +
+    @Query(joinQuery+
+            whereName +
             "AND c.categoryId = :catId "+
             "AND s.itemPrice >= :minPrice"
     )
     List<ItemDTO> findItemsByNameLikeAndSameCatMin(@Param("name") String name,@Param("catId") short catId ,@Param("minPrice")int minPrice);
 
-    @Query("SELECT new com.ecommerce.itemservice.dto.ItemDTO(" +
-            "new com.ecommerce.itemservice.dto.ItemInfo(i.itemId, c.categoryId, i.itemName, i.regDt), " +
-            "new com.ecommerce.itemservice.dto.SalesInfoDTO(s.salesInfoId, s.sellerId, s.itemCount, s.itemPrice, s.itemStatus)) " +
-            "FROM Item i " +
-            "JOIN i.category c " +
-            "JOIN i.salesInfo s " +
-            "WHERE i.itemName LIKE concat('%', :name, '%') " +
+    @Query(joinQuery+
+            whereName +
             "AND c.categoryId = :catId "+
             "AND s.itemPrice >= :minPrice "+
             "AND s.itemPrice <= :maxPrice"
     )
     List<ItemDTO> findItemsByNameLikeAndSameCatMinMax(@Param("name") String name,@Param("catId") short catId,@Param("minPrice")int minPrice,@Param("maxPrice")int maxPrice );
-    @Query("SELECT new com.ecommerce.itemservice.dto.ItemDTO(" +
-            "new com.ecommerce.itemservice.dto.ItemInfo(i.itemId, c.categoryId, i.itemName, i.regDt), " +
-            "new com.ecommerce.itemservice.dto.SalesInfoDTO(s.salesInfoId, s.sellerId, s.itemCount, s.itemPrice, s.itemStatus)) " +
-            "FROM Item i " +
-            "JOIN i.category c " +
-            "JOIN i.salesInfo s " +
-            "WHERE c.categoryId = :catId "+
+    @Query(joinQuery+
+            whereName+
             "AND s.itemPrice >= :minPrice "+
             "AND s.itemPrice <= :maxPrice"
     )
     List<ItemDTO> findItemsByCatMinMax(@Param("catId") short catId,@Param("minPrice")int minPrice,@Param("maxPrice")int maxPrice );
-    @Query("SELECT new com.ecommerce.itemservice.dto.ItemDTO(" +
-            "new com.ecommerce.itemservice.dto.ItemInfo(i.itemId, c.categoryId, i.itemName, i.regDt), " +
-            "new com.ecommerce.itemservice.dto.SalesInfoDTO(s.salesInfoId, s.sellerId, s.itemCount, s.itemPrice, s.itemStatus)) " +
-            "FROM Item i " +
-            "JOIN i.category c " +
-            "JOIN i.salesInfo s " +
+    @Query(joinQuery+
             "WHERE c.categoryId = :catId "+
             "AND s.itemPrice <= :maxPrice"
     )
     List<ItemDTO> findItemsByCatMax(@Param("catId") short catId,@Param("maxPrice")int maxPrice);
-    @Query("SELECT new com.ecommerce.itemservice.dto.ItemDTO(" +
-            "new com.ecommerce.itemservice.dto.ItemInfo(i.itemId, c.categoryId, i.itemName, i.regDt), " +
-            "new com.ecommerce.itemservice.dto.SalesInfoDTO(s.salesInfoId, s.sellerId, s.itemCount, s.itemPrice, s.itemStatus)) " +
-            "FROM Item i " +
-            "JOIN i.category c " +
-            "JOIN i.salesInfo s " +
+    @Query(joinQuery+
             "WHERE c.categoryId = :catId "+
             "AND s.itemPrice >= :minPrice"
     )

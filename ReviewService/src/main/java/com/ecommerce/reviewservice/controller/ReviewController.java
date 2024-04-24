c89b4491ebffde3dfe6d31aef7dab117a0789f7d
Heemin Woo <heem6woo@gmail.com>
2024-04-24 11:06:25 +0900

    Feat: List and write reviews endpoints added

--- ReviewService/src/main/java/com/ecommerce/reviewservice/controller/ReviewController.java
+++ ReviewService/src/main/java/com/ecommerce/reviewservice/controller/ReviewController.java
@@ -1,9 +1,12 @@
 package com.ecommerce.reviewservice.controller;
 
+import com.ecommerce.reviewservice.dto.ReviewDto;
 import com.ecommerce.reviewservice.entity.Review;
 import com.ecommerce.reviewservice.dto.CustomerDto;
 import com.ecommerce.reviewservice.service.ReviewService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,34 +15,52 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/api/reviews")
 public class ReviewController {
-
-    private final MemberFeign memberFeign;
-    private final ItemFeign itemFeign;
 
     @Autowired
     private final ReviewService reviewService;
 
 
+    // for all
     @GetMapping("/items/{item-name}")
     public ResponseEntity<List<Review>> listAllReviewsByItem(@PathVariable(name = "item-name") String itemName) {
-        //아이템 서비스에 rest로 상품 아이디 요청
-        int itemId = (int) itemFeign.retrieveItemId(itemName);
-
         //해당 상품의 모든 리뷰 쿼리 findAllByItemId
-        return ResponseEntity.ok(reviewService.findAllByItemId(itemId));
+        return ResponseEntity.ok(reviewService.findAllByItemName(itemName));
     }
 
 
     // only for admin??
     @GetMapping("/customers/{email}")
-    public ResponseEntity<List<Review>> listAllReviewsOfCustomer(@PathVariable(name = "email") String userEmail) {
-//         멤버 서비스에 rest로 멤버 아이디 요청
-        int customerId = memberFeign.retrieveCustomerId(userEmail).getId();
+    public ResponseEntity<List<Review>> listAllReviewsOfCustomer(@PathVariable(name = "email")String userEmail) {
 //         해당 멤버의 모든 리뷰 쿼리 findAllByCustomerId
-        return ResponseEntity.ok(reviewService.findAllByCustomerId(customerId));
+        return ResponseEntity.ok(reviewService.findAllByCustomerEmail(userEmail));
+    }
+
+
+    // for customer
+    @GetMapping("/list")
+    public ResponseEntity<List<Review>> listAllReviewsOfCustomer(HttpServletRequest httpServletRequest) {
+//         멤버 서비스에 rest로 멤버 아이디 요청
+        log.info(httpServletRequest.getHeader("Authorization"));
+        log.info(httpServletRequest.getHeader("email"));
+        String userEmail = httpServletRequest.getHeader("email");
+//         해당 멤버의 모든 리뷰 쿼리 findAllByCustomerId
+        return ResponseEntity.ok(reviewService.findAllByCustomerEmail(userEmail));
+    }
+
+
+    // for customer
+    @PostMapping("/item/{item_name}")
+    public ResponseEntity<String> writeReview(HttpServletRequest httpServletRequest,
+                                              @RequestBody ReviewDto reviewRequest,
+                                              @PathVariable(name = "item_name") String item_name) {
+
+        String userEmail = httpServletRequest.getHeader("email");
+
+        return ResponseEntity.ok(reviewService.saveReview(userEmail, item_name,  reviewRequest));
     }
 
 

643d969d1f2a35569052ae3cf4c4ab1d82d682e3
Heemin Woo <heem6woo@gmail.com>
2024-04-25 20:10:44 +0900

    Order Service added

---
+++ OrderService/src/main/java/com/ecommerce/orderservice/controller/OrderController.java
@@ -0,0 +1,9 @@
+package com.ecommerce.orderservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+public class OrderController {
+}

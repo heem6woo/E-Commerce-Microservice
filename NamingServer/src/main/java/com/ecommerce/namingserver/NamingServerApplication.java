4f722ef3ab7fae618db23828ffd3ebe57ccf1e12
Heemin Woo <heem6woo@gmail.com>
2024-05-27 14:31:11 +0900

    Feat: CI test

--- NamingServer/src/main/java/com/ecommerce/namingserver/NamingServerApplication.java
+++ NamingServer/src/main/java/com/ecommerce/namingserver/NamingServerApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @EnableEurekaServer
 public class NamingServerApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(NamingServerApplication.class, args);
     }
